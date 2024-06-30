243ed46db1cfe76a14cb8c2613400e156b77fe72
risunyaaa <risunyaaa@yandex.ru>
2024-06-30 21:23:28 +0300

    quick fix

--- tg-mini-app/vite.config.ts
+++ tg-mini-app/vite.config.ts
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+// Загрузить переменные окружения из файла `.env`
+dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 443,
-    host: "0.0.0.0",
-    hmr: {
-        host: 'tg-mini-app.local',
-        port: 443,
-    },
-    https: {
-      key: fs.readFileSync('./.cert/localhost-key.pem'),
-      cert: fs.readFileSync('./.cert/localhost.pem'),
-    },
-  },
-})
+  define: {
+    'process.env': process.env
+  }
+});
